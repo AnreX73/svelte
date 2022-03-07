49d8b863cc9909a9d01def546da30dc8a4d13131
AnreX73 <han220173@gmail.com>
2022-03-07 18:28:22 +0700

    апдейт опросов

--- my-app/src/routes/polls/PollDetail.svelte
+++ my-app/src/routes/polls/PollDetail.svelte
@@ -1,15 +1,37 @@
 <script>
 import PollCard from "$lib/PollCard.svelte";
+import {PollStore} from "./../stores/pollstore.js";
 
   export let poll; 
   $: totalVotes = poll.votesA + poll.votesB;
+
+  
+
+  const upVotes = (option,id)=>{
+    PollStore.update(currentPoll =>{
+    let copiedPool = [...currentPoll];
+    let upvotedPoll = copiedPool.find((poll)=>poll.id===id)
+
+    if(option === 'a'){
+        upvotedPoll.votesA++;
+    }
+    if(option === 'b'){
+        upvotedPoll.votesB++;
+    }
+
+    return copiedPool
+
+
+  })
+}   
 </script>
+
 <PollCard>
 <div class="poll"> 
     <p>Количество голосов {totalVotes}</p>      
     <h3>{poll.question}</h3>
-    <div class="answer a-color"><span>{poll.answerA}  ({poll.votesA})</span></div>
-    <div class="answer b-color">{poll.answerB} <span>({poll.votesB})</span></div>
+    <div class="answer a-color" on:click={upVotes('a',poll.id)}><span>{poll.answerA}  ({poll.votesA})</span></div>
+    <div class="answer b-color" on:click={upVotes('b',poll.id)}> <span>{poll.answerB} ({poll.votesB})</span></div>
 </div>
 </PollCard>
 <style>
