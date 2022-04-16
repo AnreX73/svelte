159213524d060ccc6176882d59ddc1e146f5e0e2
AnreX73 <han220173@gmail.com>
2022-04-16 16:15:25 +0700

    пробы с fetch

--- my-app/src/routes/stores/mainstore.js
+++ my-app/src/routes/stores/mainstore.js
@@ -5,16 +5,18 @@ const fetchSmart = async () => {
 	const url = 'http://127.0.0.1:8000/api/v1/smartlist/';
 	const res = await fetch(url);
 	const data = await res.json();
+	console.log(data);
+}
 	
-	const loadedSmart = data.results.map((data,index) => {
-		return {
-			name: data.title,
-			id: data.id,
-			image_url: data.menu_image
-		};
-	});
-	Smart.set(loadedSmart);
+// 	const loadedSmart = data.results.map((data) => {
+// 		return {
+// 			name: data.title,
+// 			id: data.id
+			
+// 		};
+// 	});
+// 	Smart.set(loadedSmart);
 	
-};
+// };
 
 fetchSmart();
