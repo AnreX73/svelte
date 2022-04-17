8484392ad56c71e64387b7a901c53df2312bc18a
AnreX73 <han220173@gmail.com>
2022-04-17 08:58:08 +0700

    drf работает

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -6,7 +6,12 @@
 <h1  >MAIN PAGE  1</h1>
 <div class="all-smart"  >
     {#each $Smart as smart_item (smart_item.id)}
+        {smart_item.id}
         {smart_item.name}
+        <img class="nav_image serv-image"
+        src={smart_item.image_url}
+        alt={smart_item.name}
+      />
     
     {/each}  
 </div>
